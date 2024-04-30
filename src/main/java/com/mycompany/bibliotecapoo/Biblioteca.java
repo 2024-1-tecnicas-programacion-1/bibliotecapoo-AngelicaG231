18444339464eb56d663c9b357f2eb927571a073c
AngelicaG231 <maria.garzonm@konradlorenz.edu.co>
2024-04-29 21:49:48 -0500

    Biblioteca

--- src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
+++ src/main/java/com/mycompany/bibliotecapoo/Biblioteca.java
@@ -1,5 +1,36 @@
 package com.mycompany.bibliotecapoo;
 
 public class Biblioteca {
-    // TODO: Aquí va tu código
+    private LinkedList<Libro> libros;
+
+    public Biblioteca() {
+        this.libros = new LinkedList<>();
+    }
+
+    public void registrarLibro(Libro libro) {
+        libros.add(libro);
+    }
+
+    public Libro buscarLibro(String palabraBusqueda) { 
+        for (Libro libro : libros) {
+            if (libro.mostrarInformacion().toLowerCase().contains(palabraBusqueda.toLowerCase())) {
+                return libro;
+            }
+        }
+        return null;
+    }
+
+    public void mostrarLibros() {
+        for (Libro libro : libros) {
+            System.out.println(libro.mostrarInformacion());
+        }
+    }
+
+    public void mostrarLibrosNoLeidos() { 
+        for (Libro libro : libros) {
+            if (!libro.isLeido()) {
+                System.out.println(libro.mostrarInformacion());
+            }
+        }
+    }
 }
